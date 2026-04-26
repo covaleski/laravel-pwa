4afbb8b1ab172ae888fb52f288a8de775030e609
covaleski <covaleski23@gmail.com>
2026-04-26 16:52:43 -0300

    refactor: do repeat yourself sometimes

--- custom/Providers/BladeServiceProvider.php
+++ custom/Providers/BladeServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Covaleski\LaravelPwa\Providers;
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -36,21 +35,29 @@ class BladeServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Blade::directive('pwaContainer', function (string $expression) {
-            return $this->makeAttributeMergeDirective(
-                "['hx-headers' => json_encode(['X-Csrf-Token' => csrf_token()])]",
-                "config('pwa.attributes.container')",
-                $expression,
-            );
+            return <<<PHP
+                <?php echo \Illuminate\Support\Arr::toHtmlAttributes(
+                    array_merge_recursive(
+                        ['hx-headers' => json_encode(['X-Csrf-Token' => csrf_token()])],
+                        config('pwa.attributes.container'),
+                        collect({$expression})->all(),
+                    ),
+                ); ?>
+                PHP;
         });
         Blade::directive('pwaOverlay', function (string $expression) {
-            return $this->makeAttributeMergeDirective(
-                "config('pwa.attributes.overlay')",
-                $expression,
-            );
+            return <<<PHP
+                <?php echo \Illuminate\Support\Arr::toHtmlAttributes(
+                    array_merge_recursive(
+                        config('pwa.attributes.overlay'),
+                        collect({$expression})->all(),
+                    ),
+                ); ?>
+                PHP;
         });
         Blade::directive('pwaPage', function (string $expression) {
             /** @var \Illuminate\View\Compilers\BladeCompiler $this */
-            $this->footer[] = '<?php echo $__env->stopFragment(); ?>';
+            $this->footer[] = "<?php echo \$__env->stopFragment(); ?>";
             return <<<PHP
                 <?php \$__env->startFragment('shell'); ?>
                 <?php echo strval({$expression}); ?>
@@ -59,32 +66,25 @@ class BladeServiceProvider extends ServiceProvider
                 PHP;
         }, true);
         Blade::directive('pwaScript', function (string $expression) {
-            return $this->makeAttributeMergeDirective(
-                "config('pwa.attributes.script')",
-                $expression,
-            );
+            return <<<PHP
+                <?php echo \Illuminate\Support\Arr::toHtmlAttributes(
+                    array_merge_recursive(
+                        config('pwa.attributes.script'),
+                        collect({$expression})->all(),
+                    ),
+                ); ?>
+                PHP;
         });
         Blade::directive('pwaShell', function (string $expression) {
-            return $this->makeAttributeMergeDirective(
-                "['hx-get' => \$url]",
-                "config('pwa.attributes.shell')",
-                $expression,
-            );
+            return <<<PHP
+                <?php echo \Illuminate\Support\Arr::toHtmlAttributes(
+                    array_merge_recursive(
+                        ['hx-get' => \$url],
+                        config('pwa.attributes.shell'),
+                        collect({$expression})->all(),
+                    ),
+                ); ?>
+                PHP;
         });
-    }
-
-    /**
-     * Create a directive to merge HTML attributes from array input.
-     */
-    protected function makeAttributeMergeDirective(string ...$expressions)
-    {
-        return sprintf(
-            '<?php echo %s::toHtmlAttributes(array_merge_recursive(%s)); ?>',
-            Arr::class,
-            collect($expressions)
-                ->transform(fn ($v) => $v ?: '[]')
-                ->map(fn ($e) => "(array) {$e},")
-                ->join(' '),
-        );
     }
 }
